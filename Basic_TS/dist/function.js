dd637f990be86c656021a157d35cb2352e722347
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-10-02 22:32:03 +0600

    TypeScript basics — variables, type safety, arrays, objects, and function

---
+++ Basic_TS/dist/function.js
@@ -0,0 +1,6 @@
+// function multiply(a, b){
+//     return a*b;
+// }
+export {};
+// console.log(multiply(2, 3)); //Wrong
+//# sourceMappingURL=function.js.map
--- Basic_TS/src/script.ts
+++ Basic_TS/src/script.ts
@@ -1,2 +1,62 @@
 const city = "Dhaka city";
 console.log(city);
+
+
+/*==============================*/
+let name = "Tasnuva";
+
+name = 243; //cannot assign number
+
+console.log(name);
+
+
+/*==============================*/
+let play;
+
+play = "Football";
+play = 243; //can assign number
+
+console.log(play);
+
+
+// Fuctions
+/*===============================*/
+function multiply(a, b){
+    return a*b;
+}
+
+console.log(multiply(2, "tasnu")); //Run but Wrong
+
+/*===============================*/
+function multiply2(a:number, b:number){
+    return a*b;
+}
+
+console.log(multiply2(2, "tasnu")); //Giving error
+console.log(multiply2(2, 4));
+
+
+// Arrays
+/*===============================*/
+let games = ["Ludu", "Caram"];
+
+games.push(345); // Would not take
+
+
+/*===============================*/
+let gamesArr = ["Ludu", "Caram", 55];
+
+gamesArr.push(345); // will take
+
+
+// Objects
+/*===============================*/
+let person = {
+    name : "Mila",
+    age : 25,
+    isFemale: true
+}
+
+person.name = 454 // error
+person.country = "BD" // error
+person.isFemale = false // Correct
