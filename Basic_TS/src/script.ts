40ffbe18718974fd1083af841af1c52ae31e0cff
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-10-03 18:42:22 +0600

    Function type, optional parameter, default parameter, return type define of Typescript

--- Basic_TS/src/script.ts
+++ Basic_TS/src/script.ts
@@ -139,3 +139,45 @@ person3 = {
     name: 344,
     age: "45"
 }
+
+
+
+// Function type
+/*===============================*/
+let myFunc: Function;
+
+myFunc = () =>{
+    console.log("Hiii");
+}
+
+const myFunc2 = (a: string, b: string, c:string) =>{
+    console.log(a, b);
+}
+
+// myFunc2("hello", " world"); //Error
+myFunc2("hello", "world", "guys");
+
+
+// Optional parameter
+const myFunc3 = (a: string, b: string, c?:string) =>{
+    console.log(a, b);
+}
+
+myFunc3("hello", " world"); //Correct (c is now optional)
+
+// Default value
+const myFunc4 = (a: string, b: string, c:string = "Guys") =>{
+    console.log(a, b);
+}
+
+myFunc4("hello", " world"); //Correct (c has a default value)
+myFunc4("hello", " world", "Tasnuva"); //Correct 
+
+
+// Return type define
+const myFunc5 = (a: string, b: string) : string => {
+    return (a + b);
+}
+
+myFunc5("Hello", " World");
+
